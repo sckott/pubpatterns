533dbdf99a579cebd0f689a2279323d6f2edf706
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-01 12:09:29 -0700

    added a pensoft file, reworked to new format updated spec and example updated readme with link to test suite

--- README.md
+++ README.md
@@ -17,8 +17,16 @@ Use publisher URL patterns to generate full text links to scholarly articles.
 
 All `.json` files are in the [src/](src/) directory.
 
+## Test suite
+
+Tests are in [test/](test/) - will be run on Travis-CI on each change in this repo.
+
+Tests are meant to make sure that URLs actually work. Failures will quickly
+let us know we need to fix something.
+
 ## Contribute
 
-Please do contribute! Fork, add a `.json` file, send a pull request to this repo. Thanks!
+Please do contribute! Fork, make a feature branch, add a `.json` file in `src/`,
+add a test in `test/`, send a pull request to this repo. Thanks!
 
 [![rofooter](http://ropensci.org/public_images/github_footer.png)](http://ropensci.org)
--- test/test-works.rb
+++ test/test-works.rb
@@ -8,6 +8,8 @@ class TestWorks < Test::Unit::TestCase
   def setup
     @elife_doi = "10.7554/eLife.07404"
     @plos_doi = '10.1371/journal.pone.0033693'
+    @nber_doi = '10.3386/w21615'
+    @peerj_doi = '10.7717/peerj.991'
     @dois = ['10.1007/12080.1874-1746','10.1007/10452.1573-5125', '10.1111/(issn)1442-9993']
   end
 
@@ -18,27 +20,45 @@ class TestWorks < Test::Unit::TestCase
       f.adapter Faraday.default_adapter
     end
 
-    res = con.get
-    assert_equal(1, res.length)
-    assert_equal(Array, res.class)
-    assert_equal(Hash, res[0].class)
+    res = conn.get
+    assert_equal(Faraday::Response, res.class)
+    assert_equal(String, res.body.class)
   end
 
-  def test_nber
-    res = Serrano.works(ids: @dois)
-    assert_equal(3, res.length)
-    assert_equal(Array, res.class)
-    assert_equal(Hash, res[0].class)
-  end
+  # def test_nber
+  #   nber = MultiJson.load(File.open('src/nber.json'))
+
+  #   conn = Faraday.new(:url => nber['urls']['xml'] % @nber_doi.match(nber['regex']).to_s) do |f|
+  #     f.adapter Faraday.default_adapter
+  #   end
+
+  #   res = conn.get
+  #   assert_equal(Faraday::Response, res.class)
+  #   assert_equal(String, res.body.class)
+  # end
 
   def test_peerj
-    res = Serrano.works(query: "ecology")
-    assert_equal(4, res.length)
-    assert_equal(Hash, res.class)
+    peerj = MultiJson.load(File.open('src/peerj2.json'))
+
+    conndoi = Faraday.new(:url => 'http://api.crossref.org/works/%s' % @peerj_doi) do |f|
+      f.adapter Faraday.default_adapter
+    end
+    issn = MultiJson.load(conndoi.get.body)['message']['ISSN'][0]
+
+    conn = Faraday.new(
+      :url =>
+        peerj['journals'].select { |x| x['issn'] == issn }[0]['urls']['xml'] %
+          @peerj_doi.match(peerj['regex']).to_s) do |f|
+      f.adapter Faraday.default_adapter
+    end
+
+    res = conn.get
+    assert_equal(Faraday::Response, res.class)
+    assert_equal(String, res.body.class)
   end
 
-  def test_plos
-    res = Serrano.works(filter: {has_funder: true, has_full_text: true})
-    assert_equal(Hash, res.class)
-  end
+  # def test_plos
+  #   res = Serrano.works(filter: {has_funder: true, has_full_text: true})
+  #   assert_equal(Hash, res.class)
+  # end
 end
