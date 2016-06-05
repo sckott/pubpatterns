fec9f66fc51be92674f4a9b7c70f8459da5a7a54
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-05 12:17:07 -0700

    improved thieme tests

--- test/test-thieme.rb
+++ test/test-thieme.rb
@@ -7,10 +7,8 @@ class TestThieme < Test::Unit::TestCase
 
   def setup
     @doi1 = '10.1055/s-0042-103414'
-    @doi2 = ''
-    @doi3 = ''
-    @doi4 = ''
-    @doi5 = ''
+    @doi2 = '10.1055/s-0036-1579629'
+    @doi3 = '10.1055/s-0042-102460'
     @thieme = MultiJson.load(File.open('src/thieme.json'))
   end
 
@@ -28,22 +26,61 @@ class TestThieme < Test::Unit::TestCase
     assert_nil(@thieme['journals'].collect{ |x| x['urls']['xml'] }.uniq[0])
   end
 
-  # def test_thieme_pdf
-  #   conndoi = Faraday.new(:url => 'http://api.crossref.org/works/%s' % @doi1) do |f|
-  #     f.adapter Faraday.default_adapter
-  #   end
-  #   issn = MultiJson.load(conndoi.get.body)['message']['ISSN'][0]
+  def test_thieme_pdf_1
+    conndoi = Faraday.new(:url => 'http://api.crossref.org/works/%s' % @doi1) do |f|
+      f.adapter Faraday.default_adapter
+    end
+    issn = MultiJson.load(conndoi.get.body)['message']['ISSN'][0]
 
-  #   conn = Faraday.new(
-  #     :url =>
-  #       @thieme['journals'].select { |x| Array(x['issn']).select{ |z| !!z.match(issn) }.any? }[0]['urls']['xml'] %
-  #         @doi.match(@thieme['journals'][0]['components']['doi']['regex']).to_s) do |f|
-  #     f.adapter Faraday.default_adapter
-  #   end
+    conn = Faraday.new(
+      :url =>
+        @thieme['journals'].select { |x| Array(x['issn']).select{ |z| !!z.match(issn) }.any? }[0]['urls']['pdf'] %
+          @doi1.match(@thieme['journals'][0]['components']['doi']['regex']).to_s) do |f|
+      f.adapter Faraday.default_adapter
+    end
 
-  #   res = conn.get
-  #   assert_equal(Faraday::Response, res.class)
-  #   assert_equal(String, res.body.class)
-  # end
+    res = conn.get
+    assert_equal(Faraday::Response, res.class)
+    assert_equal(String, res.body.class)
+    assert_equal("application/pdf;charset=utf-8", res.headers['content-type'])
+  end
+
+  def test_thieme_pdf_2
+    conndoi = Faraday.new(:url => 'http://api.crossref.org/works/%s' % @doi2) do |f|
+      f.adapter Faraday.default_adapter
+    end
+    issn = MultiJson.load(conndoi.get.body)['message']['ISSN'][0]
+
+    conn = Faraday.new(
+      :url =>
+        @thieme['journals'].select { |x| Array(x['issn']).select{ |z| !!z.match(issn) }.any? }[0]['urls']['pdf'] %
+          @doi2.match(@thieme['journals'][0]['components']['doi']['regex']).to_s) do |f|
+      f.adapter Faraday.default_adapter
+    end
+
+    res = conn.get
+    assert_equal(Faraday::Response, res.class)
+    assert_equal(String, res.body.class)
+    assert_equal("application/pdf;charset=utf-8", res.headers['content-type'])
+  end
+
+  def test_thieme_pdf_3
+    conndoi = Faraday.new(:url => 'http://api.crossref.org/works/%s' % @doi2) do |f|
+      f.adapter Faraday.default_adapter
+    end
+    issn = MultiJson.load(conndoi.get.body)['message']['ISSN'][0]
+
+    conn = Faraday.new(
+      :url =>
+        @thieme['journals'].select { |x| Array(x['issn']).select{ |z| !!z.match(issn) }.any? }[0]['urls']['pdf'] %
+          @doi2.match(@thieme['journals'][0]['components']['doi']['regex']).to_s) do |f|
+      f.adapter Faraday.default_adapter
+    end
+
+    res = conn.get
+    assert_equal(Faraday::Response, res.class)
+    assert_equal(String, res.body.class)
+    assert_equal("application/pdf;charset=utf-8", res.headers['content-type'])
+  end
 
 end
